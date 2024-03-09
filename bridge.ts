6ad133f41228e9de24614b5c577f96ffe4b0c94d
guomaoye <guomaoye@pingansec.com>
2024-03-09 23:46:17 +0800

    code

--- bridge.ts
+++ bridge.ts
@@ -1,1 +1,17 @@
-import iter_file from './utils'
+import {iter_file, get_file} from './utils'
+import {Account, Wallet, Signer, Address, Provider, Contract, Mnemonic} from "fuels";
+
+const endpoint = 'https://beta-5.fuel.network/graphql'
+const provider = await Provider.create(endpoint)
+
+const eth_address = ''
+const wallet_list = iter_file('wallet.txt')
+
+const contract_address = ''
+const abi = get_file('abi.json')
+const contract = new Contract(contract_address, abi, provider)
+wallet_list.forEach(element => {
+    const wallet = Wallet.fromMnemonic(element, undefined, undefined, provider)
+    console.log(wallet.address)
+    const balance = await wallet.getBalance(eth_address)
+});
--- utils.ts
+++ utils.ts
@@ -1,6 +1,11 @@
 const fs = require('fs')
 
-function iter_file(file_name: string) {
+export function iter_file(file_name: string) {
     const lines = fs.readFileSync(file_name, 'utf-8').split('\n');
     return lines
 } 
+
+export function get_file(file_name: string){
+    const content = fs.readFileSync(file_name, 'utf-8')
+    return content
+}
